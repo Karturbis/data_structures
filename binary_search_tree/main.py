4c99dd47a938cfd47c01cd818d2c6c5dbe2eff1c
karturbis <vulkanera42@gmx.ch>
2024-09-17 16:50:42 +0200

    add methods to Binary_Tree class

--- binary_search_tree/main.py
+++ binary_search_tree/main.py
@@ -8,6 +8,16 @@ class Binary_Node:
 class Binary_Tree:
     def __init__(self):
         self.root = Binary_Node("root")
+    
+    def insert_node(self, name, value):
+        #Add Node
+    
+    def delete_node(self, name):
 
-    def search_bin_tree(self, key):
-        
+    def get_value(self, key:str):
+        if key:
+        else:
+            raise ValueError
+
+if __name__ == "__main__":
+    bin_tree = Binary_Tree()

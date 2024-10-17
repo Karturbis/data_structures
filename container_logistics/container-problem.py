1a43148df80b5b1d34464317667670f0c378c9a5
karturbis <vulkanera42@gmx.ch>
2024-10-17 11:24:06 +0200

    implement unvisited parameter, finish implementing the depth_first_search

--- container_logistics/container-problem.py
+++ container_logistics/container-problem.py
@@ -1,8 +1,14 @@
 import networkx as nx
 
-def breadth_first_search(in_graph, node):
+def strip_unvisited(graph_dict: dict):
+    return {n: graph_dict[n] for n in graph_dict if graph_dict[n]["visited"]}
+
+def breadth_first_search(in_graph, node, include_unvisited=False):
+    """Returns the path to @node in @in_graph as a dict"""
     queue: list = [node]
-    node_attr = {n: {"visited": False, "parent": None, "depth": 0} for n in in_graph.nodes}
+    node_attr = {
+        n: {"visited": False, "parent": None, "depth": 0} for n in in_graph.nodes
+    }
     while queue:
         current_node = queue.pop(0)
         for neighbour in in_graph.adj[current_node]:
@@ -14,15 +20,22 @@ def breadth_first_search(in_graph, node):
             na["parent"] = current_node
             na["depth"] = node_attr[current_node]["depth"] + 1
 
+        if not include_unvisited:
+            node_attr = strip_unvisited(node_attr)
         return node_attr
 
-def depth_first_search(in_graph, node):
+def depth_first_search(in_graph, node, include_unvisited=False):
     stack = [node]
-    node_attr = {n: {"visited": False, "parent": None, "depth": 0, "discovery_time": 0, "end_time": 0} for n in in_graph.nodes}
-    time = 0
+    node_attr = {
+        n: {
+            "visited": False,
+            "parent": None,
+            "depth": 0,
+        }
+        for n in in_graph.nodes
+    }
     while stack:
         current_node = stack.pop(-1)
-        time += 1
         for neighbour in in_graph.adj[current_node]:
             if node_attr[neighbour]["visited"]:
                 continue
@@ -30,12 +43,17 @@ def depth_first_search(in_graph, node):
             na = node_attr[neighbour]
             na["visited"] = True
             na["parent"] = current_node
-            na["depth"] = node_attr[current_node]["depth"] +1
-            na["discovery_time"] = time
+            na["depth"] = node_attr[current_node]["depth"] + 1
     
-        return node_attr
+    
+
+    if not include_unvisited:
+        node_attr = strip_unvisited(node_attr)
+    return node_attr
+
 
 if __name__ == "__main__":
     n = 10
-    p = 3/n
+    p = 3 / n
     Graph = nx.gnp_random_graph(n, p)
+    print(breadth_first_search(Graph, 2))

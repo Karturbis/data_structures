9cc4ac34e5c398bde258f12252332967819bb14c
karturbis <vulkanera42@gmx.ch>
2024-11-05 14:13:09 +0100

    fix bug, start node not having distance 0

--- container_logistics/container-problem.py
+++ container_logistics/container-problem.py
@@ -1,8 +1,12 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+import pprint
+
 
 def strip_unvisited(graph_dict: dict):
     """return the Graph, without all unvisited nodes"""
     return {n: graph_dict[n] for n in graph_dict if graph_dict[n]["visited"]}
+
 
 def breadth_first_search(in_graph, node, include_unvisited=False):
     """Returns the path to @node in @in_graph as a dict"""
@@ -10,6 +14,7 @@ def breadth_first_search(in_graph, node, include_unvisited=False):
     node_attr = {
         n: {"visited": False, "parent": None, "depth": 0} for n in in_graph.nodes
     }
+    node_attr[node]["visited"] = True
     while queue:
         current_node = queue.pop(0)
         for neighbour in in_graph.adj[current_node]:
@@ -21,9 +26,10 @@ def breadth_first_search(in_graph, node, include_unvisited=False):
             na["parent"] = current_node
             na["depth"] = node_attr[current_node]["depth"] + 1
 
-        if not include_unvisited:
-            node_attr = strip_unvisited(node_attr)
-        return node_attr
+    if not include_unvisited:
+        node_attr = strip_unvisited(node_attr)
+    return node_attr
+
 
 def depth_first_search(in_graph, node, include_unvisited=False):
     stack = [node]
@@ -35,6 +41,7 @@ def depth_first_search(in_graph, node, include_unvisited=False):
         }
         for n in in_graph.nodes
     }
+    node_attr[node]["visited"] = True
     while stack:
         current_node = stack.pop(-1)
         for neighbour in in_graph.adj[current_node]:
@@ -45,8 +52,6 @@ def depth_first_search(in_graph, node, include_unvisited=False):
             na["visited"] = True
             na["parent"] = current_node
             na["depth"] = node_attr[current_node]["depth"] + 1
-    
-    
 
     if not include_unvisited:
         node_attr = strip_unvisited(node_attr)
@@ -54,7 +59,11 @@ def depth_first_search(in_graph, node, include_unvisited=False):
 
 
 if __name__ == "__main__":
-    n = 10
+    SEED = 42
+    n = 12
     p = 3 / n
-    Graph = nx.gnp_random_graph(n, p)
-    print(breadth_first_search(Graph, 2))
+    Graph = nx.gnp_random_graph(n, p, seed=SEED)
+    pprint.pprint(depth_first_search(Graph, 2))
+    pos = nx.spring_layout(Graph, seed=SEED)
+    nx.draw(Graph, pos, with_labels=True)
+    plt.show()
